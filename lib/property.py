4e5d8541bb37fceed1a45787b06b162951f2a181
Patrick Skipworth <pskipworth@fastmail.com>
2024-03-04 17:09:52 +0000

    Property repository and test

--- lib/property.py
+++ lib/property.py
@@ -1,7 +1,6 @@
 class Property():
 
-    def __init__(self, id, name, description, cost_per_night, user_id):
-        self.id = id
+    def __init__(self, name, description, cost_per_night, user_id):
         self.name = name
         self.description = description
         self.cost_per_night = cost_per_night
