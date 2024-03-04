d76aac2e1ad6d52657d622e060cd0748752aa5a7
Patrick Skipworth <pskipworth@fastmail.com>
2024-03-04 16:28:38 +0000

    Property class and tests

---
+++ lib/property.py
@@ -0,0 +1,14 @@
+class Property():
+
+    def __init__(self, id, name, description, cost_per_night, user_id):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.cost_per_night = cost_per_night
+        self.user_id = user_id
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+    
+    def __repr__(self):
+        return f'Property details: {self.name}, {self.description}, £{self.cost_per_night}'
