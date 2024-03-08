6e796ff241677d501b417977f665d9b29cafe35a
Venera <zhargakovavenera@gmail.com>
2024-03-08 17:54:47 +0000

    home page and property list page design

--- tests/test_user_repository.py
+++ tests/test_user_repository.py
@@ -48,23 +48,7 @@ def test_delete(db_connection):
         User(1, 'Venera', 'venera@gmail.com', 'venera123', "07463648536"),
         User(2, 'Andre', 'andre@gmail.com', 'andre123', "07463228136"),
         User(3, 'Booker', 'booker@gmail.com', 'booker123', "01163228136")
-    ]
-
-def test_create_existsing_user_will_raise_error(db_connection):
-    db_connection.seed('seeds/makersbnb_seed.sql')
-    user_repo = UserRepository(db_connection)
-    with pytest.raises(Exception) as e:
-        user_repo.create(User(None, 'Venera', 'venera@gmail.com', 'venera123', "07463648536"))
-    error_message = str(e.value)
-    assert error_message == "User already exists. Choose a new username."
-
-def test_password_is_valid_will_raise_error(db_connection):
-    db_connection.seed('seeds/makersbnb_seed.sql')
-    user_repo = UserRepository(db_connection)
-    with pytest.raises(Exception) as e:
-        user_repo.create(User(None, 'Matt', 'venera@gmail.com', 'venera123', "07463648536"))
-    error_message = str(e.value)
-    assert error_message == "Password is not valid: password must be minimum 8 characters long and contain one of the following: '!@$%&'"
+    ] 
 
 #  test find all properties from user
 #  it might make more sense to have as a location rather than user?? 
